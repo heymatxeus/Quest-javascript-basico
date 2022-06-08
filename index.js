560e89be333cf9ada467c5f2eb57a1c66bf77198
Mateus Gomes <heymatxeus@gmail.com>
2022-06-08 19:17:56 -0300

    Upload files

---
+++ index.js
@@ -0,0 +1,27 @@
+function somando(numero1, numero2){
+    return numero1 + numero2;
+}
+
+const resultadoSoma = somando(25, 25);
+console.log(resultadoSoma);
+
+function multiplicando(numero1, numero2){
+    return numero1 * numero2;
+}
+
+const resultadoMultiplicacao = multiplicando(25, 25);
+console.log(resultadoMultiplicacao);
+
+function subtraindo(numero1, numero2){
+    return numero1 - numero2;
+}
+
+const resultadoSubtracao = subtraindo(25, 25);
+console.log(resultadoSubtracao);
+
+function dividindo(numero1, numero2){
+    return numero1 / numero2;
+}
+
+const resultadoDivisao = dividindo(25, 25);
+console.log(resultadoDivisao);
